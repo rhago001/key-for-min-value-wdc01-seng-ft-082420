990534387cc228f4c5d5b06597acd85a87bbe354
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-12 20:31:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+result=""
 
+ name_hash.each do |key, value|
+   value=:key.min()
+   
+
+end
+puts value
 end

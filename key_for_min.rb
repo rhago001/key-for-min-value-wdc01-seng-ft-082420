07402909baddb4793ca5440ab807c2a2a2409b80
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-12 20:39:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,17 @@
 
 
 
-
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
 
 
 

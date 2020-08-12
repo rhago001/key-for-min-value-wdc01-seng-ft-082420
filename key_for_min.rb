4dbd543d4e2670c1f1009165ad3abd580935f9e1
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-12 20:35:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,11 @@
 def key_for_min_value(name_hash)
 result=""
 
- name_hash.each do |key, value|
-   value=:key.min()
-   name_hash.each_key { |key_value_array| block }
+# name_hash.each do |key, value|
+   #value=:key.min()
+  # name_hash.each_key { |key_value_array| block }
+  name_hash.sort {|key, value| key <=> value} 
+  puts key
 
 end
 puts value
